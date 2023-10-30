cff01b9fa45e4207d9451f6e13c5ab5f7e514dbb
Sam Martin <swordeh@users.noreply.github.com>
2023-10-30 16:05:16 +0000

    Add test for CheckFileExtensionIsAllowed.

---
+++ files_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestCheckExtensionIsAllowed(t *testing.T) {
+
+	var tests = []struct {
+		filename string
+		want     bool
+	}{
+		{"file.mp4", true},
+		{"file.jpg", true},
+		{"file.exe", false},
+	}
+
+	for _, tt := range tests {
+		testname := fmt.Sprintf("%v", tt.filename)
+		t.Run(testname, func(t *testing.T) {
+			ans := CheckExtensionIsAllowed(tt.filename)
+			if ans != tt.want {
+				t.Errorf("CheckExtensionIsAllowed(\"%v\") = %v; want %v", tt.filename, ans, tt.want)
+			}
+		})
+	}
+
+}
