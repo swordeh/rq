40f9f4b8af42e09771408777a71d7aed54417fc7
Sam Martin <swordeh@users.noreply.github.com>
2023-11-22 16:11:40 +0000

    Implement middleware in http server to inject IDs

--- server.go
+++ server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -18,6 +19,19 @@ type RqRequest struct {
 	RqId string `json:"id"`
 }
 
+func RqHttpMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+
+		rqid := GenerateRequestId()
+		ctx := req.Context()
+		ctx = context.WithValue(ctx, "rqid", rqid)
+		req = req.WithContext(ctx)
+		log.Printf("processing request %v", rqid)
+
+		next.ServeHTTP(w, req)
+	})
+}
+
 // ReturnHTTPError returns an ErrorResponse back to the client if a request has failed.
 func ReturnHTTPErrorResponse(w http.ResponseWriter, errorMessage string, status int) {
 	w.WriteHeader(status)
@@ -29,14 +43,17 @@ func GenerateRequestId() string {
 	return uuid.New().String()
 }
 
+//func HttpOnlyHandler(w http.ResponseWriter, req *http.Request) {
+//	req.Context()
+//	rqid := GenerateRequestId()
+//	rq := RqRequest{
+//		RqId: rqid,
+//	}
+//}
+
 func QueueMediaHandler(w http.ResponseWriter, req *http.Request) {
-
-	rqid := GenerateRequestId()
-	rq := RqRequest{
-		RqId: rqid,
-	}
-
-	log.Println("Received a request: ", rq)
+	rqidCtx := req.Context().Value("rqid")
+	rqid := fmt.Sprint(rqidCtx)
 
 	file, headers, err := req.FormFile("file")
 	if err != nil {
@@ -44,8 +61,8 @@ func QueueMediaHandler(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	srcFileName := headers.Filename
-	dstFileName := rq.RqId
+	srcFileName := headers.Filenameq
+	dstFileName := rqid
 	err = StoreFile(dstFileName, srcFileName, file)
 	if err != nil {
 		var errMsg string
@@ -63,7 +80,7 @@ func QueueMediaHandler(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	output, err := json.Marshal(rq)
+	output, err := json.Marshal(RqRequest{RqId: rqid})
 	io.WriteString(w, string(output))
 }
 
@@ -72,7 +89,14 @@ func main() {
 	if err := LoadConfigFile("default"); err != nil {
 		log.Fatal("Could not load config file", err)
 	}
-	http.HandleFunc("/api/rq/request", QueueMediaHandler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+
+	mux := http.NewServeMux()
+
+	MediaRequestHandler := http.HandlerFunc(QueueMediaHandler)
+	mux.Handle("/api/rq/request", RqHttpMiddleware(MediaRequestHandler))
+
+	//http.HandleFunc("/api/rq/request", QueueMediaHandler)
+	//htto.HandleFunc("/api/rq/http", HttpOnlyHandler)
+	log.Fatal(http.ListenAndServe(":8080", mux))
 
 }
