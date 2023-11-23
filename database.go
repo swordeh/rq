af9649ea8bccad1d7b3b590bbf465e01551c6ee4
Sam Martin <swordeh@users.noreply.github.com>
2023-11-23 19:27:09 +0000

    Add initial strucure for db handling.

---
+++ database.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"errors"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
+type RqRecord struct {
+	gorm.Model
+	RqId   string
+	Method string
+	Url    string
+	Error  string
+}
+
+func OpenDatabase() error {
+	switch config.Database.Engine {
+	case "sqlite":
+		conn, err := gorm.Open(sqlite.Open(config.Database.Filepath))
+		if err != nil {
+			return err
+		}
+
+		db = conn
+		migrateSchema()
+
+	default:
+		return errors.New("no or unsupported database driver defined")
+	}
+
+	return nil
+
+}
+
+func migrateSchema() {
+	db.AutoMigrate(&RqRecord{})
+}
+
+func CreateRequest(request RqRequest) {
+
+}
