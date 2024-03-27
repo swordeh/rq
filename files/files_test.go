f9c8be4e7dd668a9c4343aaf893786f3d6eaec24
Sam Martin <swordeh@users.noreply.github.com>
2024-03-27 08:03:44 +0000

    Update file extension check function. Add in-memory filestorage for tests. Update tests.

--- files/files_test.go
+++ files/files_test.go
@@ -3,8 +3,17 @@ package files
 import (
 	"fmt"
 	"rq/config"
+	"rq/records"
 	"testing"
 )
+
+type InMemoryRecordStore struct {
+	db []records.RqRecord
+}
+
+func (rs *InMemoryRecordStore) Add(record records.RqRecord) {
+	rs.db = append(rs.db, record)
+}
 
 func TestCheckExtensionIsAllowed(t *testing.T) {
 	config.Config.PermittedFileExtensions = "mp4|jpg"
@@ -15,75 +24,22 @@ func TestCheckExtensionIsAllowed(t *testing.T) {
 	}{
 		{"file.mp4", true, "mp4"},
 		{"file.jpg.mp4", true, "mp4"},
-		{"file.jpg.mp4.exe", false, ""},
+		{"file.jpg.mp4.exe", false, "exe"},
 		{"file.jpg", true, "jpg"},
-		{"file.exe", false, ""},
-		{"file.sh", false, ""},
-		{"file", false, ""},
+		{"file.exe", false, "exe"},
+		{"file.sh", false, "sh"},
+		{"file", false, "file"},
 	}
 
 	for _, tt := range tests {
 		testname := fmt.Sprintf("%v", tt.filename)
 		t.Run(testname, func(t *testing.T) {
-			ans, ext := CheckExtensionIsAllowed(tt.filename)
+			ans, ext := CheckExtensionIsAllowed(tt.filename, config.Config.PermittedFileExtensions)
 			if ans != tt.wantBool || ext != tt.wantString {
-				t.Errorf("CheckExtensionIsAllowed(\"%v\") = %v, \"%v\"; want %v, %v", tt.filename, ans, ext, tt.wantBool, tt.wantString)
+				t.Errorf("CheckExtensionIsAllowed(\"%v\") = %v, \"%v\"; want %v, %v",
+					tt.filename, ans, ext, tt.wantBool, tt.wantString)
 			}
 
 		})
 	}
-
 }
-
-//func TestStoreFile(t *testing.T) {
-//	config.Config.UploadDirectory, _ = os.Getwd() // Look at using os.Executable() instead
-//	config.Config.PermittedFileExtensions = "mp4|jpg"
-//
-//	data := []byte("test file")
-//	reader := bytes.NewReader(data)
-//
-//	type args struct {
-//		filename       string
-//		sourceFilename string
-//		file           io.Reader
-//	}
-//	tests := []struct {
-//		name    string
-//		args    args
-//		wantErr bool
-//	}{
-//		{
-//			name:    "good video",
-//			args:    args{filename: "test.mp4", sourceFilename: "file.mp4", file: reader},
-//			wantErr: false,
-//		},
-//		{
-//			name:    "bad video",
-//			args:    args{filename: "test.exe", sourceFilename: "file.exe", file: reader},
-//			wantErr: true,
-//		},
-//	}
-//	for _, tt := range tests {
-//		t.Run(tt.name, func(t *testing.T) {
-//			err := StoreFile(tt.args.filename, tt.args.sourceFilename, tt.args.file)
-//			//fmt.Println("Error != nil is ", err != nil, "and tt.wantErr is ", tt.wantErr, "and if statement is", (err != nil) != tt.wantErr)
-//
-//			// If (there is an error present) that is != to what we tt.wantErr
-//			if (err != nil) != tt.wantErr {
-//				t.Errorf("StoreFile() error = %v, wantErr %v", err, tt.wantErr)
-//				return
-//			}
-//			if err == nil {
-//				// Check the function behaved and actually created the file.
-//				absoluteFilePath := fmt.Sprintf("%v/%v", config.Config.UploadDirectory, tt.args.filename)
-//				if _, err = os.Stat(absoluteFilePath); errors.Is(err, os.ErrNotExist) {
-//					t.Errorf("StoreFile() error = %v", err)
-//					return
-//				}
-//				// remove test file
-//				os.Remove(absoluteFilePath)
-//			}
-//
-//		})
-//	}
-//}
